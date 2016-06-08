c4799c19475f1d59c037f2d10beb0107edfc0f2e
Reinaldo Ferreira <reinaldocesarferreira@gmail.com>
2016-06-08 02:48:06 +0100

    simulating classes and objects

--- chronometer.js
+++ chronometer.js
@@ -1,62 +1,80 @@
-var secondsUpdated = 0;
-var minutesUpdated = 0;
-var hoursUpdated = 0;
+Chronometer = function() {
+  this.secondsUpdated = 0;
+  this.minutesUpdated = 0;
+  this.hoursUpdated = 0;
+  this.timing = undefined;
+  this.toggleOnOff = false;
+}
+
+Chronometer.prototype = {
+
+  changeElement: function(elementToChange, value) {
+    value = value < 10 ? "0" + value : value
+    document.getElementById(elementToChange).innerHTML = value;
+  },
+
+  countingSeconds: function() {
+    var that = this;
+
+    this.timing = setTimeout(function() {
+      that.countingSeconds();
+    }, 1000);
+
+    if (this.secondsUpdated < 59) {
+      this.secondsUpdated += 1;
+    } else {
+      this.secondsUpdated = 0;
+
+      if (this.minutesUpdated < 59) {
+        this.minutesUpdated += 1;
+        this.changeElement("minutes", this.minutesUpdated);
+      } else {
+        this.minutesUpdated = 0;
+        this.hoursUpdated += 1;
+        this.changeElement("hours", this.hoursUpdated);
+      };
+    };
+
+    this.changeElement("seconds", this.secondsUpdated);
+  },
+
+  reset:function(){
+    this.secondsUpdated = 0;
+    this.minutesUpdated = 0;
+    this.hoursUpdated = 0;
+    this.changeElement("seconds", 0);
+    this.changeElement("minutes", 0);
+    this.changeElement("hours", 0);
+  },
+
+  resume: function(){
+    console.log('start/resume');
+    if (!this.toggleOnOff) {
+      this.toggleOnOff = true;
+      this.countingSeconds();
+    }
+  },
+
+  stop: function(){
+    console.log('stop');
+    clearTimeout(this.timing);
+    this.toggleOnOff = false;
+  }
+}
+
 var btnStart = document.getElementById("start");
 var btnStop = document.getElementById("stop");
 var btnReset = document.getElementById("reset");
-var counter = 0;
-var timing;
-var toggleOnOff = false;
-
-function changeElement(elementToChange, value) {
-  value = value < 10 ? "0" + value : value
-  document.getElementById(elementToChange).innerHTML = value;
-};
-
-var countingSeconds = function() {
-  timing = setTimeout(function() {
-    countingSeconds();
-  }, 100);
-
-  if (secondsUpdated < 59) {
-    secondsUpdated += 1;
-  } else {
-    secondsUpdated = 0;
-
-    if (minutesUpdated < 59) {
-      minutesUpdated += 1;
-      changeElement("minutes", minutesUpdated);
-    } else {
-      minutesUpdated = 0;
-      hoursUpdated += 1;
-      changeElement("hours", hoursUpdated);
-    };
-  };
-
-  changeElement("seconds", secondsUpdated);
-};
+var my_chronometer = new Chronometer();
 
 btnStart.onclick = function() {
-  console.log('start');
-
-  if (!toggleOnOff) {
-    toggleOnOff = true;
-    countingSeconds();
-  };
+  my_chronometer.resume()
 };
 
 btnStop.onclick = function() {
-  console.log('stop');
-  clearTimeout(timing);
-  toggleOnOff = false;
+  my_chronometer.stop()
 };
 
 btnReset.onclick = function() {
-  console.log('reset');
-  secondsUpdated = 0;
-  minutesUpdated = 0;
-  hoursUpdated = 0;
-  changeElement("seconds", secondsUpdated);
-  changeElement("minutes", minutesUpdated);
-  changeElement("hours", hoursUpdated);
+  my_chronometer.reset()
 };
